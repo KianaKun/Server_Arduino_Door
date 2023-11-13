539230a49d5b17b7faaff2c820f67d3b5d4f255f
Kiana <63056431+KianaKun@users.noreply.github.com>
2023-11-13 07:32:32 +0700

    Update action.php

--- action.php
+++ action.php
@@ -1,9 +1,12 @@
+// File untuk mengupdate status di index.php lewat db
 <?php
+// Mengecheck apakah ada data variabel smartdoor dan status didalam file index.php
     if(isset($_GET['smartdoor']) && isset($_GET['status'])){
         include ('connectdb.php');
+// Memasukan data ke variabel $status dan $smartdoor
         $status=$_GET['status'];
         $smartdoor=$_GET['smartdoor'];
-
+// Kondisi jika variabel smartdoor pada index.php itu 1, maka statusnya akan di update dan datanya diubah sesuai dengan $status yg ditekan di web.
         if($smartdoor=='1'){
             mysqli_query($dbconnect, "UPDATE kontrol SET door='$status'");
         }
