6ac5d76fca7208d6973463ad935763c7b17f126d
Kiana <63056431+KianaKun@users.noreply.github.com>
2023-11-13 07:16:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,17 +7,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Controlling Arduino</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
     <nav class="navbar navbar-light bg-light">
         <div class="container">
-            <span class="navbar-brand mb-0 h1">Latihan</span>
+                <!--      Judul Website        -->
+            <span class="navbar-brand mb-0 h1">Controlling Smart Door</span> 
         </div>
     </nav>
-
+    
+    <!-- Konek DB MYSQL Untuk checking Status -->
     <div class="container">
         <?php
             $sql = mysqli_query($dbconnect, "SELECT * FROM kontrol");
@@ -30,9 +32,10 @@
                 <div class="card-body">
                     <h4 class="card-title">
                         <?php 
-                            $status = ($row['door'] == '0') ? "OFF" : "ON";
-                            echo $status;
+                            $status = ($row['door'] == '0') ? "OFF" : "ON"; #Kondisi Checking Status ON OFF Pada Bar
+                            echo $status; #Inisiasi/Output Status
                         ?>
+                    <!--          button action on/off        -->
                     </h4>
                     <a href="action.php?smartdoor=1&status=1" class="btn btn-success">ON</a>
                     <a href="action.php?smartdoor=1&status=0" class="btn btn-danger">OFF</a>
