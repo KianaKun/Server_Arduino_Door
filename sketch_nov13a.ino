27ae1cbcb85d37009386bd3be344feea16ece823
Kiana <63056431+KianaKun@users.noreply.github.com>
2023-11-14 11:46:46 +0700

    Update sketch_nov13a.ino

--- sketch_nov13a.ino
+++ sketch_nov13a.ino
@@ -3,13 +3,13 @@
 #include <ArduinoJson.h>
 #include <ESP32Servo.h>
 
-String key = "83217";
+String key = "83217"; //Token pada database mysql
 const int servo = 25;
 int derajat = 0;
 Servo myServo;
 
-const char* ssid = "Salwa Firodisa";
-const char* password = "Cemong30";
+const char* ssid = "SSID";
+const char* password = "Password";
 
 void setup(){
     Serial.begin(115200);
@@ -27,14 +27,14 @@ void setup(){
     }
 
     Serial.println("\nConnected to the WiFi network");
-    Serial.print("Local ESP32 IP: ");
+    Serial.print("IP Addressnya: ");
     Serial.println(WiFi.localIP());
 }
 
 void loop(){
   if(WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
-    http.begin("http://192.168.1.2/control.php");
+    http.begin("http://192.168.136.9/control.php");
     http.addHeader("Content-type", "application/x-www-form-urlencoded");
     int httpResponseCode = http.POST("token="+ key);
     delay(100);
@@ -46,19 +46,19 @@ void loop(){
       deserializeJson(doc,json);
       int door = doc["door"];
       if(door == 1){
-        derajat=180;
+        derajat=100;
         myServo.write(derajat);
+        Serial.println("Pintu terkunci");
       }else{
         derajat=0;
         myServo.write(derajat);
+        Serial.println("Pintu Terbuka");
       }
-      Serial.print("door =");
-      Serial.println(door);
     }else{
-      Serial.print("Error on sending POST");
+      Serial.print("Koneksi ke database : ");
       Serial.println(httpResponseCode);
     }
     http.end();
   }
-  delay(5000);
+  delay(3000);
 }
